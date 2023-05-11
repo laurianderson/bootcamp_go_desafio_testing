c4bc19b94d2434a750081feabad916dc6be3b69b
laurianderson <laura.anderson@mercadolibre.com>
2023-05-11 13:38:34 -0300

    feat: add service mock

--- internal/products/service_mock.go
+++ internal/products/service_mock.go
@@ -1,1 +1,24 @@
 package products
+
+import "github.com/stretchr/testify/mock"
+
+
+// NewServiceMock is a builder and creates a new implementation of ServiceMock
+func NewServiceMock(repoMock *RepositoryMock) *ServiceMock {
+	return &ServiceMock{
+		repositoryMock: *repoMock,
+	}
+}
+
+
+// ServiceMock is and struct and contains an embeding package mock
+type ServiceMock struct {
+	mock.Mock
+	repositoryMock RepositoryMock
+}
+
+
+// GetAllBySeller calls to repository mock and get all by seller
+func (s *ServiceMock) GetAllBySeller(sellerID string) ([]Product, error) {
+	return s.repositoryMock.GetAllBySeller(sellerID)
+}
