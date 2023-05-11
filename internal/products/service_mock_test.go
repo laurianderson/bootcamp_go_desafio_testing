a8aba1575bab40ea69d1bd5ce420ddd3b2d8afcc
laurianderson <laura.anderson@mercadolibre.com>
2023-05-11 13:56:22 -0300

    feat: test service get sellers case success

---
+++ internal/products/service_mock_test.go
@@ -0,0 +1,28 @@
+package products
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServiceMock_GetSellers(t *testing.T) {
+	//succed case
+	t.Run("get sellers", func(t *testing.T) {
+		//arrange
+		storage := NewRepositoryMock()
+		storage.On("GetAllBySeller", "FEX112AC" ).Return([]Product{
+			{ID: "mock", SellerID: "FEX112AC", Description: "generic product", Price:123.55},
+		} ,nil)
+
+		sv := NewService(storage)
+		//act
+		seller, err := sv.GetAllBySeller("FEX112AC")
+
+		//assert
+		assert.NoError(t, err)
+		assert.Equal(t, []Product{
+			{ID: "mock", SellerID: "FEX112AC", Description: "generic product", Price:123.55},
+		}, seller)
+	})
+}
